ebc30f79f88473eb36fc6eb0cec23a22676febca
nabind47 <nabindhami619@gmail.com>
2024-12-14 15:42:07 +0545

    feat: defining models and validations

---
+++ internal/handler/model.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"time"
+)
+
+type NewsPostRequestBody struct {
+	Author    string   `json:"author"`
+	Title     string   `json:"title"`
+	Summary   string   `json:"summary"`
+	CreatedAt string   `json:"created_at"`
+	Content   string   `json:"content"`
+	Source    string   `json:"source"`
+	Tags      []string `json:"tags"`
+}
+
+func (n NewsPostRequestBody) Validate() (errs error) {
+	if n.Author == "" || n.Title == "" || n.Summary == "" || n.CreatedAt == "" || n.Content == "" || n.Source == "" || len(n.Tags) == 0 {
+		return fmt.Errorf("invalid payload: one or more required fields are empty")
+	}
+
+	if _, err := time.Parse(time.RFC3339, n.CreatedAt); err != nil {
+		errs = errors.Join(errs, fmt.Errorf("invalid created_at format: %w", err))
+	}
+
+	if _, err := url.ParseRequestURI(n.Source); err != nil {
+		errs = errors.Join(errs, fmt.Errorf("invalid source URL: %w", err))
+	}
+
+	return errs
+}
---
+++ internal/handler/model_test.go
@@ -0,0 +1,74 @@
+package handler_test
+
+import (
+	"testing"
+
+	"github.com/nabind47/go_rest47/internal/handler"
+)
+
+func TestNewsPostRequestBody_Validate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		body     handler.NewsPostRequestBody
+		expected bool
+	}{
+		{
+			name:     "All fields empty - expect error",
+			body:     handler.NewsPostRequestBody{},
+			expected: true,
+		},
+		{
+			name: "Invalid CreatedAt format - expect error",
+			body: handler.NewsPostRequestBody{
+				Author:    "Author",
+				Title:     "Title",
+				Summary:   "Summary",
+				CreatedAt: "InvalidDate",
+				Content:   "Content",
+				Source:    "http://example.com",
+				Tags:      []string{"tag1"},
+			},
+			expected: true,
+		},
+		{
+			name: "Invalid Source URL - expect error",
+			body: handler.NewsPostRequestBody{
+				Author:    "Author",
+				Title:     "Title",
+				Summary:   "Summary",
+				CreatedAt: "2024-12-14T15:04:05Z",
+				Content:   "Content",
+				Source:    "invalid_url",
+				Tags:      []string{"tag1"},
+			},
+			expected: true,
+		},
+		{
+			name: "Valid payload - no error expected",
+			body: handler.NewsPostRequestBody{
+				Author:    "Author",
+				Title:     "Title",
+				Summary:   "Summary",
+				CreatedAt: "2024-12-14T15:04:05Z",
+				Content:   "Content",
+				Source:    "http://example.com",
+				Tags:      []string{"tag1", "tag2"},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.body.Validate()
+
+			if tc.expected && err == nil {
+				t.Fatalf("expected error but got nil")
+			}
+
+			if !tc.expected && err != nil {
+				t.Fatalf("expected no error but got: %v", err)
+			}
+		})
+	}
+}
